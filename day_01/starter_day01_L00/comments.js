1f8c231db75e35421bc18351e1516c8f7fe3a353
gregory <graigtech@gmail.com>
2022-02-15 13:28:28 +0400

    22

--- day_01/starter_day01_L00/comments.js
+++ day_01/starter_day01_L00/comments.js
@@ -1,1 +1,6 @@
 // Exercices: Data Types --> Level00 [1,2,3]
+//Les commentaires peuvent rendre le code lisible
+//Bienvenue dans JavaScript
+//Les commentaires peuvent rendre le code lisible,
+// facile à réutiliser
+// et informatif
--- day_01/starter_day01_L00/variable.js
+++ day_01/starter_day01_L00/variable.js
@@ -1,18 +1,21 @@
 // *****Exercices: Data Types --> Level00 [4]*****
+let premierVar = "tout debut";
+let deuxio = undefined;
+let troisio = false;
+let quatro = null;
 
+let var1 = "";
+let var2 = "";
+let var3 = "";
+let var4 = "";
+
+let var5 = 1;
+let var6 = 2;
+let var7 = 3;
+let var8 = 4;
 
 // Declaring different variables of different data types
 
-
-
-
 // Declaring variables with number values
 
-
-
-
-
-
 // Variables can also be declaring in one line separated by comma
-
-
