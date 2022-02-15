38feea17aded9f4416ea61e34c54b60caff38b2d
gregory <graigtech@gmail.com>
2022-02-15 13:44:05 +0400

    ajout datatype

---
+++ day_01/starter_day01_L00/datatypes.js
@@ -0,0 +1,23 @@
+// typeof
+let premierVar = "tout debut";
+typeof premierVar;
+let deuxio = ;
+typeof deuxio;
+let troisio = false;
+typeof troisio ;
+let quatro = null;
+typeof quatro
+
+
+let var1 = "";
+console.log(var1);
+let var2 = "";
+console.log(var2);
+let var3 = "";
+let var4 = "";
+
+let var5 = 1;
+console.log(var5);
+let var6 = 2;
+let var7 = 3;
+let var8 = 4;
