045ffe0948ea8392b7bc38c2aeea18e5aa39b41d
gregory <graigtech@gmail.com>
2022-02-22 20:44:46 +0400

    remodif

--- day_03/Day03.js
+++ day_03/Day03.js
@@ -10,6 +10,7 @@ const products = [
   { product: "coffee", price: 10 },
   { product: "tea", price: "" },
 ];
+//---4, 5, 8, 11, 14, 16 FACULTATIf
 // ---1
 // console.log(products.map(getFullName));
 //---2
@@ -17,14 +18,51 @@ const products = [
 //---3
 // countries.forEach((element) => console.log(element));
 
-//---4 et 5 FACULTATIf
-
 //---6
 // let majus = countries.map((el) => {
 //   console.log(el.toUpperCase());
 // });
 
 //---7
-let majus = countries.map((el) => {
-  console.log(el.length);
-});
+// let countriesLength = countries.map((el) => {
+//   console.log(el.length);
+// });
+//---9
+// let nameMaj = names.map((el) => {
+//   console.log(el.toUpperCase());
+// });
+//---10
+// const result = countries.filter((laland) => laland.includes("land"));
+
+// console.log(result);
+//---12
+// const long_pays = countries.filter((long) => long.length > 6);
+
+// console.log(long_pays);
+//---13
+
+//---15
+// let acc = 0;
+// let redd = numbers.reduce((acc, x) => {
+//   return acc + x;
+// });
+// console.log(redd);
+//---17
+// explication
+//---18
+// let verif = names.some(sup7);
+// function sup7(item) {
+//   return item > 7;
+// }
+// console.log(verif);
+//--19
+
+let lala = countries.every(checkLand);
+
+function checkLand(item) {
+  return item.includes("land");
+}
+console.log(lala);
+//--20
+
+//----21
